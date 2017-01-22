64700050dabd3edd21c7fff2a424a99f7c8744ed
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2017-01-22 10:17:08 -0200

    Permite customizacao (especifica do provider) da leitura/escrita de json

--- java-restify/src/main/java/com/github/ljtfreitas/restify/http/client/message/converter/json/JsonpMessageConverter.java
+++ java-restify/src/main/java/com/github/ljtfreitas/restify/http/client/message/converter/json/JsonpMessageConverter.java
@@ -1,11 +1,15 @@
 package com.github.ljtfreitas.restify.http.client.message.converter.json;
 
 import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Map;
 
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
+import javax.json.JsonReaderFactory;
 import javax.json.JsonStructure;
+import javax.json.JsonWriterFactory;
 
 import com.github.ljtfreitas.restify.http.client.request.HttpRequestMessage;
 import com.github.ljtfreitas.restify.http.client.request.RestifyHttpMessageWriteException;
@@ -13,6 +17,18 @@ import com.github.ljtfreitas.restify.http.client.response.HttpResponseMessage;
 import com.github.ljtfreitas.restify.http.client.response.RestifyHttpMessageReadException;
 
 public class JsonpMessageConverter extends JsonMessageConverter<JsonStructure> {
+
+	private final JsonReaderFactory jsonReaderFactory;
+	private final JsonWriterFactory jsonWriterFactory;
+
+	public JsonpMessageConverter() {
+		this(Collections.emptyMap());
+	}
+
+	public JsonpMessageConverter(Map<String, ?> configuration) {
+		this.jsonReaderFactory = Json.createReaderFactory(configuration);
+		this.jsonWriterFactory = Json.createWriterFactory(configuration);
+	}
 
 	@Override
 	public boolean canRead(Type type) {
@@ -22,10 +38,10 @@ public class JsonpMessageConverter extends JsonMessageConverter<JsonStructure> {
 	@Override
 	public JsonStructure read(HttpResponseMessage httpResponseMessage, Type expectedType) throws RestifyHttpMessageReadException {
 		if (JsonArray.class.equals(expectedType)) {
-			return Json.createReader(httpResponseMessage.body()).readArray();
+			return jsonReaderFactory.createReader(httpResponseMessage.body()).readArray();
 
 		} else if (JsonObject.class.equals(expectedType)) {
-			return Json.createReader(httpResponseMessage.body()).readObject();
+			return jsonReaderFactory.createReader(httpResponseMessage.body()).readObject();
 
 		} else {
 			throw new RestifyHttpMessageReadException("Unsupported type: [" + expectedType + "]. Only JsonArray and JsonObject are supported.");
@@ -40,10 +56,10 @@ public class JsonpMessageConverter extends JsonMessageConverter<JsonStructure> {
 	@Override
 	public void write(JsonStructure json, HttpRequestMessage httpRequestMessage) throws RestifyHttpMessageWriteException {
 		if (json instanceof JsonArray) {
-			Json.createWriter(httpRequestMessage.output()).writeArray((JsonArray) json);
+			jsonWriterFactory.createWriter(httpRequestMessage.output()).writeArray((JsonArray) json);
 
 		} else if (json instanceof JsonObject) {
-			Json.createWriter(httpRequestMessage.output()).writeObject((JsonObject) json);
+			jsonWriterFactory.createWriter(httpRequestMessage.output()).writeObject((JsonObject) json);
 
 		} else {
 			throw new RestifyHttpMessageReadException("Unsupported type: [" + json.getClass() + "]. Only JsonArray and JsonObject are supported.");
