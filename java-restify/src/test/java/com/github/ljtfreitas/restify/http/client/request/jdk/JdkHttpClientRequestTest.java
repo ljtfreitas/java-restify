860428d9617576267bc1a66643eb427635451ed8
Tiago de Freitas Lima <ljtfreitas@gmail.com>
2017-03-21 21:22:27 -0300

    Modifica a prioridade de escolha do JsonConverter, para dar preferência ao JacksonJsonConverter (se estiver disponível no classpath)

--- java-restify/src/test/java/com/github/ljtfreitas/restify/http/client/request/jdk/JdkHttpClientRequestTest.java
+++ java-restify/src/test/java/com/github/ljtfreitas/restify/http/client/request/jdk/JdkHttpClientRequestTest.java
@@ -24,6 +24,7 @@ import org.mockserver.client.server.MockServerClient;
 import org.mockserver.junit.MockServerRule;
 import org.mockserver.model.HttpRequest;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.ljtfreitas.restify.http.RestifyHttpException;
 import com.github.ljtfreitas.restify.http.RestifyProxyBuilder;
 import com.github.ljtfreitas.restify.http.contract.BodyParameter;
@@ -209,13 +210,16 @@ public class JdkHttpClientRequestTest {
 	@XmlAccessorType(XmlAccessType.FIELD)
 	public static class MyModel {
 
+		@JsonProperty
 		String name;
+
+		@JsonProperty
 		int age;
 
 		public MyModel() {
 		}
 
-		public MyModel(String name, int age) {
+		public MyModel(@JsonProperty String name, @JsonProperty int age) {
 			this.name = name;
 			this.age = age;
 		}
